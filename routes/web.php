308400d904c24fb482b3bb3e8ec5152c7de4e924
chathuri2020 <chathu9935@gmail.com>
2024-09-25 11:41:54 +0530

    passport issues fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+Auth::routes();
+// routes/api.php
+use App\Http\Controllers\Auth\RegisterController;
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('register', [RegisterController::class, 'register']);
